b9eb6d0ab3893efcbe41876160649527ef0ee50c
Reid Burke <me@reidburke.com>
2008-11-05 22:06:16 -0800

    Using EpiCurl, link regex

--- ungadget.php
+++ ungadget.php
@@ -1,21 +1,24 @@
 <?php
 
+require('EpiCurl.php');
+
 class Ungadget {
 
     static $opensocial_version = '0.8';
 
     static function fromUrl($url) {
-        $gadget = self::makeRequest($url);
-        return self::flatten($gadget);
+        $hdl = self::getCurlHandleForUrl($url);
+        $ec = EpiCurl::getInstance();
+        $ec->addCurl($hdl);
+        $gadget = $ec->getResult((string)$hdl);
+        return self::flatten($gadget['data']);
     }
 
-    static function makeRequest($url) {
+    static function getCurlHandleForUrl($url) {
         $hdl = curl_init();
         curl_setopt($hdl, CURLOPT_URL, $url);
         curl_setopt($hdl, CURLOPT_RETURNTRANSFER, 1);
-        $res = curl_exec($hdl);
-        curl_close($hdl);
-        return $res;
+        return $hdl;
     }
 
     static function flatten($gadget) {
@@ -33,8 +36,17 @@ class Ungadget {
         if ($content) {
             // TODO consolidate scripts, styles
             $content = $content[0];
-            $script_tags = preg_match("/<script[^<>]+src=['\"]([^<>'\"]+)['\"][^<>]*>(.*)<\/script>/i", $content, $script_matches);
+            $script_tags = preg_match_all("/<script[^<>]+src=['\"]([^<>'\"]+)['\"][^<>]*>(.*)<\/script>/i", $content, $script_matches);
 var_dump($script_matches);
+            $content = str_replace($script_matches[0], '', $content);
+            foreach($script_matches[1] as $url) {
+                print $url;
+            }
+             
+            // This could do better by checking for the rel attribute...
+            $link_tags = preg_match_all("/<link[^<>]+href=['\"]([^<>'\"]+.css)['\"][^<>]*[\/]*>((<\/link>)*)/i", $content, $link_matches);
+var_dump($link_tags);
+            $content = str_replace($link_matches[0], '', $content);
             return $content;
         } else {
             $inline = $xml->xpath('/Module/Content[@type="html-inline"]');
