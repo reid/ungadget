5254585e2443a60ba6bff6fbdcc585e4372463a4
Reid Burke <me@reidburke.com>
2008-11-06 12:50:14 -0800

    Welcome message, less code

--- includes/Ungadget.php
+++ includes/Ungadget.php
@@ -11,7 +11,7 @@ class Ungadget {
         $hdl = $this->getCurlHandleForUrl($url);
         $ec = EpiCurl::getInstance();
         $ec->addCurl($hdl);
-        $gadget = $ec->getResult((string)$hdl);
+        $gadget = $ec->getResult((string) $hdl);
         return $this->transformGadget($gadget['data']);
     }
 
@@ -31,62 +31,58 @@ class Ungadget {
         $version = $this->opensocial_version;
         if ($version) {
             $opensocial = $xml->xpath('/Module/ModulePrefs/Require[@feature="opensocial-' . $version . '"]');
-            if (count($opensocial) == 0) {
+            if (count($opensocial) == 0)
                 throw new Exception('This gadget does not support OpenSocial ' . $version);
-            }
         }
 
         $content = $xml->xpath('/Module/Content[@type="html"]');
+
         if ($content) {
             $ec = EpiCurl::getInstance();
+            $hdls = array();
+            foreach(array('js', 'css') as $type) $hdls[$type] = array();
+            $content = $content[0];
 
             /* scripts */
-            $content = $content[0];
-            $script_tags = preg_match_all("/<script[^<>]+src=['\"]([^<>'\"]+)['\"][^<>]*>(.*)<\/script>/i", $content, $script_matches);
-            $script_keys = array();
-            foreach ($script_matches[1] as $url) {
+            preg_match_all("/<script[^<>]+src=['\"]([^<>'\"]+)['\"][^<>]*>(.*)<\/script>/i", $content, $scripts);
+            $content = str_replace($scripts[0], '', $content);
+            foreach ($scripts[1] as $url) {
                 $hdl = $this->getCurlHandleForUrl($url);
                 $ec->addCurl($hdl);
-                $script_keys[] = (string) $hdl;
+                $hdls['js'][] = (string) $hdl;
             }
-            $content = str_replace($script_matches[0], '', $content);
 
             /* stylesheets */
-            $link_tags = preg_match_all("/<link[^<>]+href=['\"]([^<>'\"]+.css)['\"][^<>]*[\/]*>((<\/link>)*)/i", $content, $link_matches); // This could do better by checking for the rel attribute...
-            $stylesheet_keys = array();
-            foreach ($link_matches[1] as $url) {
+            preg_match_all("/<link[^<>]+href=['\"]([^<>'\"]+.css)['\"][^<>]*[\/]*>((<\/link>)*)/i", $content, $links); // This could do better by checking for the rel attribute...
+            $content = str_replace($links[0], '', $content);
+            foreach ($links[1] as $url) {
                 $hdl = $this->getCurlHandleForUrl($url);
                 $ec->addCurl($hdl);
-                $stylesheet_keys[] = (string) $hdl;
+                $hdls['css'][] = (string) $hdl;
             }
-            $content = str_replace($link_matches[0], '', $content);
 
             /* append results */
-            $js = '';
-            $style = '';
-            foreach ($script_keys as $key) {
-                $res = $ec->getResult($key);
-                $res = $res['data'];
-                if ($res) $js .= $res;
-            }
-            foreach ($stylesheet_keys as $key) {
-                $res = $ec->getResult($key);
-                $res = $res['data'];
-                if ($res) $style .= $res;
-            }
-            if ($js) $content = '<script>' . $js . '</script>' . $content;
-            if ($content) $content = '<style>' . $style . '</style>' . $content;
+            $js = $css = '';
 
+            foreach ($hdls as $key => $ids) {
+                foreach ($ids as $id) {
+                    $res = $ec->getResult($id);
+                    $res = $res['data'];
+                    if ($res) $$key .= $res;
+                }
+            }
+
+            if ($js) $content = '<script>' . $js . '</script>' . $content;
+            if ($css) $content = '<style>' . $css . '</style>' . $content;
             if ($this->strip_newlines) $content = str_replace("\n", '', $content);
 
             return $content;
         } else {
             $inline = $xml->xpath('/Module/Content[@type="html-inline"]');
-            if ($inline) {
+            if ($inline)
                 return $inline[0];
-            } else {
+            else
                 throw new Exception('Content not found');
-            }
         }
     }
 
--- ungadget.php
+++ ungadget.php
@@ -1,5 +1,12 @@
 <?php
 
+if (!$_GET['url']) {
+    echo "<h1>ungadget</h1>\n";
+    echo "<p>Specify a gadget URL like <pre>?url=http://my.appspot.com/gadget.xml</pre></p>\n";
+    echo "<p><a href=\"http://github.com/reid/ungadget\">Grab this on GitHub</a></p>\n";
+    die();
+}
+
 require_once 'includes/Ungadget.php';
 
 $o = new Ungadget();
