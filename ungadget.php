5675d47dff672516a3734bc1f20e9ca5bde239a8
Reid Burke <me@reidburke.com>
2008-11-06 17:57:48 -0800

    Change params, more HTML

--- ungadget.php
+++ ungadget.php
@@ -1,10 +1,24 @@
 <?php
 
 if (!$_GET['url']) {
-    echo "<h1>ungadget</h1>\n";
-    echo "<p>Specify a gadget URL like <pre>?url=http://my.appspot.com/gadget.xml</pre></p>\n";
-    echo "<p>Helpful params: <ul><li>newlines=1 &mdash; don't strip newlines</li><li>ver=0.7 &mdash; allow another opensocial version</li></ul>\n";
-    echo "<p><a href=\"http://github.com/reid/ungadget\">Grab this on GitHub</a></p>\n";
+    echo <<<EOHTML
+<html>
+<head><title>ungadget</title></head>
+<body>
+<h1>ungadget</h1>
+<p>Specify a gadget URL like <pre>?url=http://my.appspot.com/gadget.xml</pre></p>
+<h2>Helpful GET Paramaters</h2>
+<dl>
+    <dt>newlines=0</dt>
+    <dd>strip newlines</dd>
+    <dt>ver=0.7</dt>
+    <dd>allow another opensocial version</dd>
+</dl>
+<h2>Why not</h2>
+<p><a href=\"http://github.com/reid/ungadget\">Grab this on GitHub</a></p>
+</body>
+</html>
+EOHTML;
     die();
 }
 
